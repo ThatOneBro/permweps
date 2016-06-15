5bd0c48070dacacaaef25d2bc8cd44bede77efe4
Derrick Farris <derrick.c.farris@gmail.com>
2016-06-15 11:22:59 -0500

    Edit parenthesis format, add extra shop button

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,43 +1,53 @@
--- local function AddonLoader()
-	-- this should test connection to MYSQL database
--- end
+--[[
+function Entity:IsPermWep( )
+	if table.HasValue( self )
+end
+]]
 
+local function DBLoader( )
+--registerDarkRPVar
+end
+hook.Add( "DarkRPDBInitialized", "PermWepsDBLoader", DBLoader )
 
-local function PermWepsNetCache()
+local function PermWepsNetCache( )
 	util.AddNetworkString( "BuyPermWep" )
 end
 hook.Add( "Initialize", "PermWepsNetCache", PermWepsNetCache )
 
-net.Receive( "BuyPermWep", function()
-	local lply = net.ReadEntity()
+net.Receive( "BuyPermWep", function( )
+	local lply = net.ReadEntity( )
+	local purchased_weapon = net.ReadString( )
 	
-	lply:Give( "weapon_ak47custom" )
+	--if not purchased_weapon:IsPermWep() then return end
+	lply:Give( purchased_weapon )
 	
 	umsg.Start( "BoughtPermWep", lply )
-	umsg.End()
+	umsg.End( )
 
 end )
 
-local function SpawnNormalGunShop()
+local function SpawnNormalGunShop( )
 	local npc = ents.Create( "permweps_shop" )
-	npc:SetPos(PermWeps.NPC1.NPCPosition)
-	npc:SetAngles(Angle(0, PermWeps.NPC1.NPCFacing, 0))
-	npc:Spawn()
-	npc:Activate()
+	npc:SetPos( PermWeps.NPC1.NPCPosition )
+	npc:SetAngles( Angle( 0, PermWeps.NPC1.NPCFacing, 0 ) )
+	npc:Spawn( )
+	npc:Activate( )
 end
 hook.Add( "InitPostEntity", "SpawnNormalGunShop", SpawnNormalGunShop )
 
-local function SpawnVIPGunShop()
-	local npc = ents.Create("permweps_vip")
-	npc:SetPos(PermWeps.NPC2.NPCPosition)
-	npc:SetAngles(Angle(0, PermWeps.NPC2.NPCFacing, 0))
-	npc:Spawn()
-	npc:Activate()
+local function SpawnVIPGunShop( )
+	local npc = ents.Create( "permweps_vip" )
+	npc:SetPos( PermWeps.NPC2.NPCPosition )
+	npc:SetAngles( Angle( 0, PermWeps.NPC2.NPCFacing, 0 ) )
+	npc:Spawn( )
+	npc:Activate( )
 end
 hook.Add( "InitPostEntity", "SpawnVIPGunShop", SpawnVIPGunShop )
 
---[[local function OnSpawnCheck( ply )
+--[[
+local function OnSpawnCheck( ply )
 	local lply = ply
 	
 	lply:SteamID()
-end]]
+end
+]]
--- lua/entities/permweps_shop/cl_init.lua
+++ lua/entities/permweps_shop/cl_init.lua
@@ -1,27 +1,43 @@
-include('shared.lua')
+include( "shared.lua" )
 
-local function PermWepsShop()
-	local pShop = vgui.Create('DFrame')
-	pShop:SetSize(400, 400)
-	pShop:SetPos(ScrW()*0.5, ScrH()*0.5)
-	pShop:SetTitle('PermWeps Shop')
-	pShop:SetSizable(true)
-	pShop:SetDeleteOnClose(false)
-	pShop:Center()
-	pShop:MakePopup()
+local function PermWepsShop( )
+	local pShop = vgui.Create( "DFrame" )
+	pShop:SetSize( 400, 400 )
+	pShop:SetPos( ScrW( )*0.5, ScrH( )*0.5 )
+	pShop:SetTitle( "PermWeps Shop" )
+	pShop:SetSizable( true )
+	pShop:SetDeleteOnClose( false )
+	pShop:Center( )
+	pShop:MakePopup( )
 	
 	local Button = vgui.Create( "DButton", pShop )
 	Button:SetText( "Buy permanent weapon!" )
 	Button:SetTextColor( Color( 255, 255, 255 ) )
 	Button:SetPos( 100, 100 )
-	Button:SetSize( 100, 30 )
+	Button:SetSize( 300, 30 )
 	Button.Paint = function( self, w, h )
 		draw.RoundedBox( 0, 0, 0, w, h, Color( 41, 128, 185, 250 ) ) -- Draw a blue button
 	end
-	Button.DoClick = function()
+	Button.DoClick = function( )
 		net.Start( "BuyPermWep" )
-			net.WriteEntity(LocalPlayer()) --Should write string and pass local player
-		net.SendToServer()
+			net.WriteEntity( LocalPlayer() ) --Should write string and pass local player
+			net.WriteString( "weapon_ak47custom" )
+		net.SendToServer( )
+	end
+	
+	local Button = vgui.Create( "DButton", pShop )
+	Button:SetText( "Buy permanent weapon!" )
+	Button:SetTextColor( Color( 255, 255, 255 ) )
+	Button:SetPos( 100, 200 )
+	Button:SetSize( 300, 30 )
+	Button.Paint = function( self, w, h )
+		draw.RoundedBox( 0, 0, 0, w, h, Color( 41, 128, 185, 250 ) ) -- Draw a blue button
+	end
+	Button.DoClick = function( )
+		net.Start( "BuyPermWep" )
+			net.WriteEntity( LocalPlayer() ) --Should write string and pass local player
+			net.WriteString( "weapon_ak47" )
+		net.SendToServer( )
 	end
 
 	chat.AddText( Color(255,255,128), PermWeps.NPC1.NPCName .. ": ", Color(255,255,255), PermWeps.NPC1.NPCPhrase )
@@ -29,7 +45,7 @@ end
 
 usermessage.Hook( "PermWepsShopUsed", PermWepsShop )
 
-local function BoughtPermWep()
+local function BoughtPermWep( )
 	chat.AddText( Color(255, 255, 255), "You bought a gun!" )
 end
 
