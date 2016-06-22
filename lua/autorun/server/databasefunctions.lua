f4e62c4df832a3fd341594825f3a30b63766fb36
Derrick Farris <derrick.c.farris@gmail.com>
2016-06-22 15:02:09 -0500

    Update db functions

--- lua/autorun/server/databasefunctions.lua
+++ lua/autorun/server/databasefunctions.lua
@@ -1,22 +1,44 @@
 local function DBLoader( )
-	MySQLite.query([[
+	MySQLite.query( [[
 		CREATE TABLE IF NOT EXISTS permweps_data(
 			steam_id BIGINT NOT NULL PRIMARY KEY,
 			owned_weps VARCHAR(800) NOT NULL,
 			equipped_weps VARCHAR(800) NOT NULL
 		);
-	]])
+	]] )
 end
 hook.Add( "DarkRPDBInitialized", "PermWepsDBLoader", DBLoader )
+
+function retrievePWepsData( ply, callback )
+
+end
 
 local meta = FindMetaTable( "Player" )
 
 function meta:CreatePWepsProfile( )
 	if not IsValid( self ) then return end
-	MySQLite.query([[
+	MySQLite.query( [[ 
 		REPLACE INTO permweps_data VALUES (]]  
 			self:SteamID64( ) .. [[, ]] ..
             MySQLite.SQLStr( "none" )  .. [[, ]] ..
 			MySQLite.SQLStr( "none" ) .. 
-		");" )
+		");" 
+	)
 end
+
+function meta:LoadPWepsProfile( )
+	if not IsValid( self ) then return end
+	MySQLite.query( [[
+		SELECT owned_weps, equipped_weps WHERE steam_id = ]]
+			..	self:SteamID64( ) ..
+		";" )
+	)
+end
+
+function meta:UpdatePWepsOwned( )
+	if not IsValid( self ) then return end
+	MySQLite.query( [[
+		UPDATE permweps_data WHERE steam_id = ]]
+			.. self:SteamID64( ) ..
+		
+	)
